3d5944b30429de26823460167316200615516441
dannyceron94 <dannyceron94@gmail.com>
2020-04-16 16:49:45 -0700

    test

--- motors.c
+++ motors.c
@@ -44,11 +44,11 @@ struct motor motor4 = {"motor4",26,10,11,26,11,10,3};
 struct motor *motor4Ptr = &motor4;
 
 
-int init(char[6],char[7]);
-int initHelper(struct motor *, char[7]);
-int foward(struct motor *,int,char[7]);
-int reverse(struct motor *,int,char[7]);
-int stop(struct motor *, char[7]);
+// int init(char[6],char[7]);
+// int initHelper(struct motor *, char[7]);
+// int foward(struct motor *,int,char[7]);
+// int reverse(struct motor *,int,char[7]);
+// int stop(struct motor *, char[7]);
 
 
 int run(int val){
