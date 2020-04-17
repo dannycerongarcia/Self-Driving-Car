63885abd38457366099e6c93c76501f89f5e1619
dannyceron94 <dannyceron94@gmail.com>
2020-04-16 17:11:10 -0700

    test

--- motors.h
+++ motors.h
@@ -1,12 +1,11 @@
+#ifndef FOO_H_
+#define FOO_H_
 
-#include <stdlib.h>
-#include <wiringPi.h>
-#include <time.h>
-#include <string.h>
-#include <stdio.h>
 int init(char[6],char[7]);
 int initHelper(struct motor *, char[7]);
 int foward(struct motor *,int,char[7]);
 int reverse(struct motor *,int,char[7]);
 int stop(struct motor *, char[7]);
 int run(int);
+
+#endif // FOO_H_
