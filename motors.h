2d40b553618cd394c3e214d6314a8afba13283db
dannyceron94 <dannyceron94@gmail.com>
2020-04-16 17:17:56 -0700

    test

--- motors.h
+++ motors.h
@@ -1,11 +1,12 @@
 #ifndef FOO_H_
 #define FOO_H_
 
+int run(int);
 int init(char[6],char[7]);
 int initHelper(struct motor *, char[7]);
 int foward(struct motor *,int,char[7]);
 int reverse(struct motor *,int,char[7]);
 int stop(struct motor *, char[7]);
-int run(int);
+
 
 #endif // FOO_H_
