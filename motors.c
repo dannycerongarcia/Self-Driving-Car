3695b9539c45c4cc4f6e931ebaddd259ed3c4d95
dannyceron94 <dannyceron94@gmail.com>
2020-05-01 19:15:52 -0700

    ...

--- motors.c
+++ motors.c
@@ -12,11 +12,11 @@
 #include <string.h>
 #include <stdio.h>
 
-#define STOP 0;
-#define FORWARD 1;
-#define LEFT 2;
-#define RIGHT 3;
-#define REVERSE 4;
+#define STOP 0
+#define FORWARD 1
+#define LEFT 2
+#define RIGHT 3
+#define REVERSE 4
 
 // #include <dos.h>
 // struct for each motor configurations
