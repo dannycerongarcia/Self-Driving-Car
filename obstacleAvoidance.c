a73187351260602a753f1a4d77e8c9024cd7e332
nsainez <34695410+nsainez@users.noreply.github.com>
2020-05-05 11:28:21 -0700

    Create obstacleAvoidance.c

---
+++ obstacleAvoidance.c
@@ -0,0 +1,50 @@
+
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define obstacle_pin 7
+
+void init();
+void loop();
+void test();
+
+int obstacle = HIGH; //high means no obstacle
+
+
+int main(void)
+{
+
+    if(wiringPiSetup() < 0){
+		printf("Initalization failed. ");
+		return 1;
+    }
+	if(wiringPiISR(obstacle_pin, INT_EDGE_FALLING, &test)<0){
+		printf("Initalization ISR failed. \n");
+		return 1;
+	}
+	
+    while(true){
+         init();
+         loop();
+    }
+}
+       
+
+void init(){
+	pinMode(obstacle_pin, INPUT);
+}
+
+void loop(){
+	obstacle = digitalRead(obstacle_pin);
+		if(obstacle == 0){
+			printf("Obstacle!!!\n");
+		} 
+		else {
+			printf("Road Clear!\n");
+		}
+	delay(500);
+}
+
+void test(){
+    printf("Obstacle Avoidance sensor test\n");
+}
