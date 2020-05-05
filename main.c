9aa484eb8276f2942f4728a3333e0bd03b703358
nsainez <34695410+nsainez@users.noreply.github.com>
2020-05-05 15:32:44 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <time.h>
 #include <string.h>
+#include <stdio.h>
+
+#include "motors.h"
+#define STOP 0
+
+int act = 1;
+int *actPtr = &act;
 
 int main() {
+   
+   run(actPtr);
+   *actPtr = STOP;
    init();
+
    printf("Hello, World!");
    return 0;
 }
