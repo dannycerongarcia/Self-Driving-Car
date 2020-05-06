8a12ee857c14637b4c011f149c48e88d5f7ebb0c
dannyceron94 <dannyceron94@gmail.com>
2020-05-05 22:33:07 -0700

    ...

--- linesensor.h
+++ linesensor.h
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <pthread.h>
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void setup_line_sensors(int, int, int);
 
 int line_sensor_loop(int *);
