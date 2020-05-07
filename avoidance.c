4bb04cba73147394886d2de831713a1147757691
nsainez <34695410+nsainez@users.noreply.github.com>
2020-05-06 18:27:10 -0700

    Update avoidance.c

--- avoidance.c
+++ avoidance.c
@@ -1,4 +1,3 @@
-
 #include <wiringPi.h>
 #include <stdio.h>
 #include "motors.h"
@@ -14,6 +13,7 @@ void test();
 int left_obs = HIGH; //high means no obstacle
 int right_obs = HIGH;
 int i = 10;
+int counter = 0;
   
 void init(){
   if(wiringPiSetup() < 0){
@@ -39,24 +39,37 @@ void setup(){
 void loop(){
 	left_obs = digitalRead(IR1);
 	right_obs = digitalRead(IR2);
-		if(left_obs == 0){
+		while(left_obs == 0){
 			printf("Obstacle to the left \n");
-			right(i);
-			delay(100);
-			allforward(i);
-			//stopAll();
-		} 
-		if(right_obs == 0){
-			printf("Obstacle to the right \n");
-			left(i);
-			delay(100);
-			allforward(i);
-			//stopAll();
-		} 
-		else {
-			printf("Road Clear.\n");
+			counter+=1;
+			printf("counter: %d\n", counter);
+			}
+			//obstacle not to the left anymore
+			if(left_obs == 1){ 
+				printf("ready to turn... \n");
+				delay(300);
+				left(counter); 
+				allforward(i);
+			}
+
+		
+		while(right_obs == 0){
+			printf("Obstacle to the left \n");
+			counter+=1;
+			printf("counter: %d\n", counter);
 		}
-         delay(300);
+			//obstacle not to the left anymore
+			if(right_obs == 1){ 
+				printf("ready to turn... \n");
+				delay(300);
+				right(counter); 
+				allforward(i);
+			}
+		
+			else {
+				printf("Road Clear.\n");
+			}
+	         delay(300);
 }
 
 void test(){
