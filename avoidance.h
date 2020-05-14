beff0f1ffce4b7c99baddb3621538dea1d3757ee
nsainez <34695410+nsainez@users.noreply.github.com>
2020-05-14 15:57:25 -0700

    Create avoidance.h

---
+++ avoidance.h
@@ -0,0 +1,16 @@
+#ifndef FOO_H_
+#define FOO_H_
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+void init_ir();
+void setup();
+void loop();
+void test();
+
+
+
+#endif // FOO_H_
