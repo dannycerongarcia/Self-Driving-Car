f840af312ade6378f7fb472ef3f479f676be8657
nsainez <34695410+nsainez@users.noreply.github.com>
2020-05-14 15:56:22 -0700

    Update avoidance.c

--- avoidance.c
+++ avoidance.c
@@ -1,77 +1,62 @@
+  
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "motors.h"
 
-#define IR1 7
-#define IR2 1
+ #define IR1 7 //RIGHT
+ #define IR2 1 //LEFT
 
 void init_ir();
 void setup();
 void loop();
 void test();
 
-int left_obs = HIGH; //high means no obstacle
-int right_obs = HIGH;
-int i = 10;
-int counter = 0;
-  
+// int left_obs = HIGH;
+// int right_obs = HIGH;
+int i = 15;
+int counter = 5;
+bool objLeft = false;
+bool objRight = false;
+
 void init_ir(){
   if(wiringPiSetup() < 0){
 		printf("Initalization failed. ");
 		return 1;
     }
-	if(wiringPiISR(obstacle_pin, INT_EDGE_FALLING, &test)<0){
-		printf("Avoidance Sensor Initalization Failed. \n");
-		return 1;
-	}
-	
-	    while(true){
-		 setup();
+	setup();
+
+	    while(1){
 		 loop();
 	    }
+	    
 }
 
 void setup(){
-	pinMode(IR1, INPUT);
 	pinMode(IR2, INPUT);
+	pinMode(IR1, INPUT);
 }
 
 void loop(){
-	left_obs = digitalRead(IR1);
-	right_obs = digitalRead(IR2);
+	int right_obs = digitalRead(IR1);
+	int left_obs = digitalRead(IR2);
 	
-		while(left_obs == 0){
-			printf("Obstacle to the left \n");
-			counter+=1;
-			printf("counter: %d\n", counter);
-		}
-			//obstacle not to the left anymore
-			if(left_obs == 1){ 
-				printf("ready to turn... \n");
-				delay(300);
-				left(counter); 
-				allForward(i);
+	printf("left_obs %d\n", left_obs);
+	printf("right_obs %d\n", right_obs);
+	
+	
+	if(left_obs == 0 || right_obs == 0){
+		printf("obstacle to the left\n");
+		allForward(i);
+		objLeft = true;
+			if(left_obs == 1 && right_obs ==1){
+				objLeft = false;
+				delay(100);
+				stopAll();
+			}	else {
+					stopAll();
 			}
-
-		
-		while(right_obs == 0){
-			printf("Obstacle to the left \n");
-			counter+=1;
-			printf("counter: %d\n", counter);
-		}
-			//obstacle not to the left anymore
-			if(right_obs == 1){ 
-				printf("ready to turn... \n");
-				delay(300);
-				right(counter); 
-				allForward(i);
-			}
-			else {
-				printf("Road Clear.\n");
-			}
-	         delay(300);
+	}
+	         delay(500);
 }
 
-void test(){
-    printf("Obstacle Avoidance test successful\n");
-}
