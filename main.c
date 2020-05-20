2390fbec017b11b61501850f5c832ae17f013f21
dmandu <43095319+dmandu@users.noreply.github.com>
2020-05-20 14:14:17 -0700

    Update main.c

--- main.c
+++ main.c
@@ -10,13 +10,18 @@
 #include "motors.h"
 #include "linesensor.h"
 #include "DistanceSensor.h"
+#include "SpeedEncoder.h"
 
 #define TRIGGER 21
 #define ECHO 22
+#define SPEEDENCODER 29
+
+void checkEchoSensor();
+void maneuverObject();
 
 int act = 0;
 int *actPtr = &act;
-pthread_t motor_thread;
+pthread_t motor_thread, speedEncoderThread;
 _Bool isTrail = TRUE;
 _Bool * isTrailPtr = &isTrail;
 //---------------------------------cleaning up threads--------------------------------
@@ -43,13 +48,57 @@ int main()
 
    setup_line_sensors(23, 24, 25);
    InitDistanceSensor(TRIGGER, ECHO);
+   InitSpeedEncoder(SPEEDENCODER);
 
    while(isTrail) {
        checkLineSensor(actPtr, isTrailPtr);
-       avoidObstacle(actPtr);
+       checkEchoSensor();
    }
 
    pthread_join(motor_thread, NULL);
    printf("Hello, World!");
    return 0;
 }
+
+
+void checkEchoSensor() {
+    printf("CHECKECHOSENSOR");
+    if(MeasureDistance() <= 10.0) {
+	    printf("POTENTIAL OBSTACLE\n");
+	    obstacle = TRUE;
+       stopAll();
+       sleep(2);
+       if(MeasureDistance() <= 10.0) {
+           printf("Obstacle still there\n");
+           maneuverObject(allMotors);
+       }
+    }
+}
+
+void maneuverObject(struct Motors allMotors []) {
+    printf("Attempting to go around\n");
+    while(digitalRead(OBSTACLESENSOR) == 1) {
+        pthread_create(&speedEncoderThread, NULL, &SpeedEncoderRotations, NULL);
+        right(40);
+        pthread_join(speedEncoderThread, NULL);
+        stopAll();
+    }
+    printf("Going forward\n");
+    while(digitalRead(OBSTACLESENSOR) == 0) {
+         allForward(23);
+    }
+    stopAll();
+    printf("Turning left\n");
+    while(digitalRead(OBSTACLESENSOR) == 1) {
+        pthread_create(&speedEncoderThread, NULL, &SpeedEncoderRotations, NULL);
+        left(40);
+        pthread_join(speedEncoderThread, NULL);
+        stopAll();
+    }
+    printf("Moving forward\n");
+    while(digitalRead(OBSTACLESENSOR) == 0) {
+        allForward(23);
+    }
+    stopAll();
+}
+
