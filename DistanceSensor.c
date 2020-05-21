ec265f1b4724aeb1680450f05e64d5a7735a5159
dmandu <43095319+dmandu@users.noreply.github.com>
2020-05-20 21:18:14 -0700

    Update DistanceSensor.c

--- DistanceSensor.c
+++ DistanceSensor.c
@@ -4,6 +4,7 @@
 #include <wiringPi.h>
 #include <time.h>
 #include "DistanceSensor.h"
+#include "motors.h"
 
 #define OBSTACLESENSOR 7
 int triggerPin, echoPin;
@@ -35,33 +36,5 @@ double MeasureDistance() {
 	distance = (time*340)/2.0;
 	printf("Distance to object: %f\n", distance*100/CLOCKS_PER_SEC);
 	return distance*100/CLOCKS_PER_SEC;
-}
-
-
-
-void avoidObstacle(int * action) {
-    if(MeasureDistance() <= 10) {
-        *action = 0;
-        sleep(3);
-        if(MeasureDistance() <= 10) {
-		maneuverObstacle(action);
-	}
-    }	    
-}
-
-
-void maneuverObstacle(int * action) {
-    while(digitalRead(OBSTACLESENSOR) == 1) {
-        *action = 3;
-    }
-    *action = 0;
-    while(digitalRead(OBSTACLESENSOR) == 0) {
-        *action = 1;
-    }
-    *action = 0;
-    while(digitalRead(OBSTACLESENSOR) == 1) {
-        *action = 2;
-    }
-    *action = 0;
 }
 
